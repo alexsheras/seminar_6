e777ab6c70fd325175dd3ead12a9b5970715433d
ALEX SHER <sheras85@mail.com>
2023-04-09 12:56:03 +0300

    задача32

---
+++ seminar_6_32.py
@@ -0,0 +1,12 @@
+import random
+array = [random.randint(-20, 20) for i in range(random.randint(10,20))]
+print(*array)
+min =int(input('Задайте минимальное значение диапазона: '))
+max =int(input('Задайте максимальное значение диапазона: '))
+result =[]
+if max>=min:
+    for i in range(len(array)):
+        if max>=array[i] and min<=array[i]:
+            result.append(i)
+
+print("Индексы элементов массива:",result)
